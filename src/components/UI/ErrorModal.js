39c7a41a31403bcc2209ea98d25b26a85cf5d1d5
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-07-28 16:13:34 +0200

    made use of Portals from react-dom to display Modal and backdrop
    If applied, this commit will have created two new roots in index.html that will be used to render Modals and Backdrops without interfering with the main root ("id=""root").
    Like this, then we use ReactDom.createPortal() to display them.
    createPortal accepts two arguments, the "what" and the "where".

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,26 +1,58 @@
+import ReactDOM from "react-dom";
 import classes from "./ErrorModal.module.css";
 import Card from "./Card";
 import Button from "./Button";
+import { Fragment } from "react";
 
+// Under to make possible the use of Portals, in this case for modal and backdrop.
+// To not interfere with the structure of the main root ("id="root""), we create Portals
+// to render them outside like an overlay.
+
+// Const that holds the backdrop and then will be displayed by the "backdrop-root"
+// in index.html by ReactDOM.createPortal() in ErrorModal const.
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onCloseModal} />;
+};
+
+// Const that holds the Modal himself and then will be displayed by the "modal-root"
+// in index.html by ReactDOM.createPortal() in ErrorModal const.
+const ModalCard = (props) => {
+  return (
+    //  Method to close modal (onCloseModal) defined in "AddUserForm" and passed here as props.
+    //  the onClick event is used both in the Button and in the backdrop div to make possible
+    //  the close possibility through clicking out of the modal as well.
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onCloseModal}>Ok!</Button>
+      </footer>
+    </Card>
+  );
+};
+
+// Under is where we call the "Portals" through ReactDOM.createPortal().
 const ErrorModal = (props) => {
   return (
-    <div>
-      {/* Method to close modal (onCloseModal) defined in "AddUserForm" and passed here as props.
-        the onClick event is used both in the Button and in the backdrop div to make possible 
-        the close possibility through clicking out of the modal as well. */}
-      <div className={classes.backdrop} onClick={props.onCloseModal} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onCloseModal}>Ok!</Button>
-        </footer>
-      </Card>
-    </div>
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onCloseModal={props.onCloseModal} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalCard
+          title={props.title}
+          message={props.message}
+          onCloseModal={props.onCloseModal}
+        />,
+        document.getElementById("modal-root")
+      )}
+      ;
+    </Fragment>
   );
 };
 
